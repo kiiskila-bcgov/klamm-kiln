73803c8d448dc80f2df5fddfb6fa21379eb41b09
brysonjbest <103070659+brysonjbest@users.noreply.github.com>
2025-06-09 16:02:14 -0700

    Fix deployment location base.

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
 
   return {
-    base: "./",
+    base: "/preview/",
     plugins: [react()],
     build: {
       target: "esnext",
